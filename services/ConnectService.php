492fab5e8155cb1e39ab2c79a957a2b6e7e1ce5a
Taylor Daughtry <taylor@caddis.co>
2015-10-16 13:42:54 -0500

    Refactor for better performance

--- services/ConnectService.php
+++ services/ConnectService.php
@@ -8,50 +8,53 @@ use \Guzzle\Http\Client;
 
 class ConnectService extends BaseApplicationComponent
 {
+	private $accessToken;
+	private $appId = '526715080828830';
+	private $appSecret = '65c392edf63064a6c8c820eeb8713083';
+	private $baseUrl = 'https://graph.facebook.com/v2.5/';
+	private $pageId = '881444838597214';
+
+	public function __construct() {
+		$this->accessToken = $this->appId . '%7C' . $this->appSecret;
+	}
 	/**
 	 * Get the most recent posts from a certain Facebook Page and wraps all
 	 * links with HTML anchor links.
 	 *
-	 * @method getPosts
-	 *
 	 * @param int $limit The number of posts to get.
-	 *
 	 * @return array Page posts with keys of 'message, created_time, id'
 	 */
 	public function getPosts($limit)
 	{
-		$appId = '526715080828830';
-		$appSecret = '65c392edf63064a6c8c820eeb8713083';
-		$baseUrl = 'https://graph.facebook.com/v2.5/';
-		$pageId = '881444838597214';
-		$accessToken = $appId . '%7C' . $appSecret;
+		$cache = craft()->cache->get('connectPosts');
 
-		if (craft()->cache->get('connectPosts')) {
-			return craft()->cache->get('connectPosts');
+		if ($cache) {
+			return $cache;
 		}
 
 		$client = new Client();
 
-		$posts = $client
-			->get($baseUrl . '/' . $pageId . '/feed?limit=' . $limit . '&access_token=' . $accessToken)
+		$request = $client
+			->get($this->baseUrl . '/' . $this->pageId . '/feed?limit=' . $limit . '&access_token=' . $this->accessToken)
 			->send()
-			->json()['data'];
+			->json();
 
-		$pattern = '/(http:\/\/[a-z0-9\.\/]+)/i';
+		$posts = $request['data'];
 
-		$replacement = '<a href="$1" target="_blank">$1</a>';
+		if ($posts) {
+			$pattern = '/(http:\/\/[a-z0-9\.\/]+)/i';
+			$replacement = '<a href="$1" target="_blank">$1</a>';
 
-		for($i = 0; $i < count($posts); $i++) {
-			$source = $posts[$i]['message'];
+			foreach ($posts as $key => $val) {
+				$posts[$key]['message'] = preg_replace($pattern, $replacement, $val['message']);
+			}
 
-			$linkedMessage = preg_replace($pattern, $replacement, $source);
+			// Cache in 5-minute increments
+			craft()->cache->set('connectPosts', $posts, 600);
 
-			$posts[$i]['message'] = $linkedMessage;
+			return $posts;
+		} else {
+			return 'Something went wrong.';
 		}
-
-		// Cache in 5-minute increments
-		craft()->cache->set('connectPosts', $posts, 600);
-
-		return $posts;
 	}
 }
